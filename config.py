cb4f6529b44d6cab52ed20966f1f32fcb3e54094
unknown <mashmanuc@gmail.com>
2024-04-25 02:09:46 +0300

    f21

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-MONGO_KEY = os.environ.get('MONGO_KEY')
+# MONGO_KEY = os.environ.get('MONGO_KEY')
+MONGO_KEY='mongodb+srv://mashmanuc:1WTFCFWcW5gbAYCU@cluster0.vbybi6i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 class Config:
  
