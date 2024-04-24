95f0863dd35c5149c7ffd1d55ec5fe1859206437
unknown <mashmanuc@gmail.com>
2024-04-25 01:27:55 +0300

    f17

--- cread_app.py
+++ cread_app.py
@@ -1,20 +1,7 @@
 
 from flask import Flask
 
-import os
-from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-
-
-
-# Створення контексту додатка перед кожним запитом
-
-
-#----------------------------------------------------------------------------#
-# App Config.
-#----------------------------------------------------------------------------#
-
